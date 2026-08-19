155ba142dc6f91131fd395c7b784ea81a1eec749
Krishiv Periwal <krishivperiwal@gmail.com>
2026-08-19 10:52:31 +0530

    Add: non-cyclical-number - submission-2

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-2.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+
+        int slow = cycle(n);
+        int fast = cycle(cycle(n));
+
+        do{
+            slow = cycle(slow);
+            fast = cycle(cycle(fast));
+            if(slow == 1 || fast == 1)return true;
+
+        }while(slow != fast);
+        return false;
+    }
+
+    int cycle(int n){
+        int ans = 0;
+        while(n > 0){
+            int dig = n%10;
+            ans += dig*dig;
+            n /= 10;
+        }
+        return ans;
+    }
+};
